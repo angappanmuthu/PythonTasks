60f5b91eec0de4e864bb02bb3300646a16d647d9
Angappan Muthu <angappanmuthu@Angappans-iMac.local>
2021-04-30 19:49:26 +0530

    second commit

--- decorator.py
+++ decorator.py
@@ -1,12 +1,37 @@
 
-import json 
+import json
+
+# function to add to JSON
+def write_json(data, filename='data.json'):
+    with open(filename,'w') as f:
+        json.dump(data, f, indent=4)
 
 json_file = open('data.json')
 
 json_data = json.load(json_file)
 
-for i in json_data:
-    print(i)
+#print(json_data)
 
-def get_each_name():
-    return ''
+# get names from the txt file using file handler
+
+names = [name.strip('\n') for name in open('name.txt','r')]
+
+
+def isNameExist(data,names):
+    for n in names:
+        for ind,d in enumerate(data):
+            if n == d['name']:
+                try:
+                    names.remove(d['name'])
+                except: 
+                    print(ind,d['name'])
+                    id = len(json_data) + 1
+                    json_data.append({"id": id, "name": d['name']})
+            #else:
+                #print(str(ind)+d["name"].center(50,"."))
+                
+    
+
+isNameExist(json_data,names)
+
+print(json_data)
